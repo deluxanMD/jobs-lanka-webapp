0eb073d425ea80efcaa2c7fa9e364a132ca76a50
Deluxan Mariathasan <deluxan_m@outlook.com>
2021-09-20 00:22:33 +0530

    linting job added to ci/cd pipeline

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,8 @@
-const reportWebVitals = onPerfEntry => {
+import { WebVitalsGlobal } from 'react';
+
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    WebVitalsGlobal.then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
